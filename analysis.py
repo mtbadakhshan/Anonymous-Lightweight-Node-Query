f39c821beda3bb32ddfd50619ef5af7d93fd493c
Taghi <mtbadakhshan@gmail.com>
2020-06-13 20:10:53 +0430

    redis db added

--- analysis.py
+++ analysis.py
@@ -5,6 +5,11 @@ import json
 import time
 import pickle
 from datetime import datetime
+import redis
+import struct # To convert bytes to array
+
+MIN_CHUNK_NUMBER = -20;
+MAX_CHUNK_NUMBER = 20;
 
 def chunk_cardinal_calculator(chunk_state):
 	chunks_cardinal = {}
@@ -20,7 +25,7 @@ def chunk_cardinal_calculator(chunk_state):
 
 def day_state_calculator(new_day, state, beta):
 	changed_chunk = 0;
-	chunks_cardinal = {x:0 for x in range(0,21)}
+	chunks_cardinal = {x:0 for x in range(-5,100)}
 	for addr in new_day:
 		if addr in state:
 			prev_chunk = determine_chunk(state[addr])
@@ -45,7 +50,118 @@ def day_state_calculator(new_day, state, beta):
 
 	return state, changed_chunk, chunks_cardinal
 
-def state_calculator(JSON_PATH, beta):
+
+def day_state_calculator_database(new_day, r_db, beta, day):
+	# changed_chunk_matrix [previous chunk][new chunk]
+	changed_chunk_matrix = {x:{y:0 for y in range(MIN_CHUNK_NUMBER, MAX_CHUNK_NUMBER)} \
+	 for x in ["nonexist", *range(MIN_CHUNK_NUMBER, MAX_CHUNK_NUMBER)]}
+	chunks_cardinal = {x:0 for x in range(MIN_CHUNK_NUMBER,MAX_CHUNK_NUMBER)}
+	
+	print("Loading from DB...")
+	with r_db.pipeline() as pipe:
+		for addr in new_day:
+			pipe.hgetall(addr)
+		prev_score_data_list = pipe.execute()
+
+	print("%d addresses loaded from DB" %len(prev_score_data_list))
+
+	new_addr = 0
+	changed_addresses = 0
+	with r_db.pipeline() as pipe:
+		for addr, prev_score_data in zip(new_day, prev_score_data_list):
+
+			new_day_datetime_object = datetime.strptime(day, '%Y%m%d')
+
+			if(prev_score_data): #Have seen the address already
+				prev_score = float(prev_score_data[b'score'].decode("utf-8"))
+				last_change = prev_score_data[b'last_change'].decode("utf-8")
+				last_change_datetime_object = datetime.strptime(last_change, '%Y%m%d')
+				day_interval = (new_day_datetime_object - last_change_datetime_object).days
+
+				# I am considering the situation, when the code reaches a data which is stored in the DB and it belongs to the future.
+				# (do not change any thing!)
+				if(day_interval<=0):
+					# print("The address %s last change belongs to a date in future or just today. Nothing changes!" %addr)
+					# print("Last Change: %s, Today: %s" %(last_change, day))
+					continue #Try the next address!
+				new_score = (1-beta) * prev_score * day_interval + beta * new_day[addr]			
+
+			else: #See the address right now for the first time.
+				prev_score = 0
+				new_score = beta * new_day[addr]
+				new_addr += 1	
+
+			# raise Exception("stop")
+
+			try:
+				prev_chunk = determine_chunk(prev_score)
+				new_chunk = determine_chunk(new_score)
+				changed_chunk_matrix[prev_chunk][new_chunk] += 1
+				chunks_cardinal[new_chunk] += 1
+			except Exception as e:
+				print("addr:", addr)
+				print("prev_chunk: ", prev_chunk)
+				print("new_score: ", new_score)
+				print("new_chunk: ", new_chunk)
+				print("changed_chunk_matrix: ", changed_chunk_matrix)
+				raise Exception("Error")
+
+			
+
+			pipe.hset(addr, "score", new_score)
+			pipe.hset(addr, "last_change", day)
+			changed_addresses += 1
+			# pipe.hmset(addr, {"score" : new_score, "last_change": day})
+
+		print("Saving %d addresses to DB" %changed_addresses)
+		pipe.execute()
+
+
+	return changed_chunk_matrix, chunks_cardinal, new_addr
+	
+
+
+# def day_state_calculator_database(new_day, r_db, beta):
+# 	changed_chunk = 0;
+# 	chunks_cardinal = {x:0 for x in range(-5,100)}
+
+# 	for addr in new_day:
+# 		if r_db.exists(addr):
+# 			prev_score = float(r_db.get(addr).decode("utf-8"))
+# 			prev_chunk = determine_chunk(prev_score)
+
+# 			new_score = (1-beta) * prev_score + beta * new_day[addr]
+# 			r_db.set(addr, new_score)
+# 			new_chunk = determine_chunk(new_score)
+
+# 			if (prev_chunk != new_chunk):
+# 				changed_chunk += 1
+# 		else:
+# 			new_score = beta * new_day[addr]
+# 			r_db.set(addr, new_score)
+# 			new_chunk = determine_chunk(new_score)
+
+# 		chunks_cardinal[new_chunk] += 1
+
+
+# 	# for addr in r_db.scan_iter("*"): 
+# 	# 	if addr not in new_day:
+# 	# 		prev_score = float(r_db.get(addr).decode("utf-8"))
+# 	# 		prev_chunk = determine_chunk(prev_score)
+
+# 	# 		new_score = (1-beta) * prev_score
+# 	# 		r_db.set(addr, new_score)
+# 	# 		new_chunk = determine_chunk(new_score);
+
+# 	# 		if (prev_chunk != new_chunk):
+# 	# 			changed_chunk += 1
+# 	# 		chunks_cardinal[new_chunk] += 1
+
+# 	return changed_chunk, chunks_cardinal
+
+
+
+def state_calculator(JSON_PATH, beta, r_db):
 	days = [x.split('.')[0] for x in os.listdir(JSON_PATH)]
 	days.sort()
 
@@ -55,34 +171,41 @@ def state_calculator(JSON_PATH, beta):
 
 	counter = 0
 	for day in days[1:]:
-		
 		with open(JSON_PATH+day+'.json', 'r') as input_file:
 			new_day = json.load(input_file)
 
 			start_time = time.time()
-			state, changed_chunk, chunks_cardinal = day_state_calculator(new_day, state, beta)
 
-			print("--- %s : %.3f seconds , %d changed , %d addresses ---" %(day, (time.time() - start_time), changed_chunk, len(state)))
+			# Memory
+			# state, changed_chunk, chunks_cardinal = day_state_calculator(new_day, state, beta)
+
+			# Data Base
+			print("------------------------------------------------------------------------------")
+			print("DATE:", day)
+
+			changed_chunk, chunks_cardinal, new_addr = day_state_calculator_database(new_day, r_db, beta, day)
+
+			print("--- %s : %.3f seconds , %d new addresses ---" %(day, (time.time() - start_time), new_addr))
 
 
-			# Refine (change this later)
-			del state['Return Operation']
-			del state['Other']
-
+			# # Refine (change this later)
+			# del state['Return Operation']
+			# del state['Other']
 
 			# Saving the objects:
 			saving_path = 'Analysis/Values/%.1f/%s' %(beta,day)
 			print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '] ' + 'Saved to \"%s'%saving_path + '.bin\"')
 			with open(saving_path + '.bin', 'wb') as f:  # Python 3: open(..., 'wb')
-   				pickle.dump([(time.time() - start_time), changed_chunk, chunks_cardinal, len(state), list(state.values())],\
+   				pickle.dump([(time.time() - start_time), changed_chunk, chunks_cardinal, new_addr],\
    				 f, protocol = pickle.HIGHEST_PROTOCOL)
    			
-			if(counter%20==0):
-				with open(saving_path + '-state' + '.json', 'w') as f:  # Python 3: open(..., 'wb')
-					json.dump(state, f)
-					print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '] ' + saving_path + '-state' + '.json - ' "State saved" )
+			# if(counter%20==0):
+			# 	with open(saving_path + '-state' + '.json', 'w') as f:  # Python 3: open(..., 'wb')
+			# 		json.dump(state, f)
+			# 		print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '] ' + saving_path + '-state' + '.json - ' "State saved" )
    			
-			counter +=1;
+			# counter +=1;
+	r_db.save()
 
 	with open(saving_path + '-state' + '.json', 'w') as f:  # Python 3: open(..., 'wb')
 		json.dump(state, f)
@@ -111,9 +234,14 @@ def json_generator(TEXT_PATH, JSON_PATH):
 
 
 def determine_chunk(score):
-	for p in range(-2,100):
+	if score == 0:
+		return "nonexist"
+
+	for p in range(MIN_CHUNK_NUMBER,MAX_CHUNK_NUMBER):
 		if score < 2**(p+1):
 			return p
+
+	return MAX_CHUNK_NUMBER - 1
 
 
 if __name__ == "__main__":
@@ -121,14 +249,15 @@ if __name__ == "__main__":
 	TEXT_PATH = 'Days/Texts/'
 	JSON_PATH = 'Days/Jsons/'
 	OUT_PATH = 'Analysis/Values/%.1f/' %(beta)
+	r_db = redis.Redis()
 
 
 	if(False):# Generate Jsons
 		json_generator(TEXT_PATH, JSON_PATH)
 
 
-	if(False):# Simulate
-		state_calculator(JSON_PATH, beta)
+	if(True):# Simulate
+		state_calculator(JSON_PATH, beta, r_db)
 
 	
 	days = [x.split('.')[0].split('-')[0] for x in os.listdir(OUT_PATH)]
